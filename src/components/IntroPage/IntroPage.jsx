948870524d7262e33109b6571d616c872ff7352c
Anthony Martinez <martinezanthony213@gmail.com>
2021-05-18 10:36:06 -0700

    commit stuff

--- src/components/IntroPage/IntroPage.jsx
+++ src/components/IntroPage/IntroPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-
+import styles from './IntroPage.module.css'
 
 export default function IntroPage(props) {
     return (
-        <div>
-            <h1>This is the {props.page}</h1>
+        <div className="intro">
+            <h1 className="title">This is the {props.page}</h1>
             <Link to='/main'>
                 <button>Play!</button>
             </Link>
