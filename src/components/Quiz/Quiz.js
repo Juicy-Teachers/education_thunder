8c9e25bd2b638398cb5a49876f14348514598c97
Susana Martins <scmartins87@gmail.com>
2021-05-18 10:26:44 -0700

    animal trivia added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -6,14 +6,105 @@ const animalQuestions = [
         'How many legs does a spider have?',
       answers: ['Six', 'Eight', 'Four', 'Two'],
       correct: 'vinayak',
-      questionId: '1'
+      questionId: 1
     },
     {
         question:
           'What’s the name of a place you go to see lots of animals?',
         answers: ['Park', 'School', 'Bus', 'Zoo'],
         correct: 'zoo',
-        questionId: '2'
+        questionId: 2
+      },
+      {
+        question:
+          'What type of fish is Nemo?',
+        answers: ['Goldfish', 'Clownfish', 'Blobfish', 'Bluefish'],
+        correct: 'Clownfish',
+        questionId: 3
+      },
+      {
+        question:
+          'What do caterpillars turn into?',
+        answers: ['Tigers', 'Monsters', 'Butterflies', 'Zebras'],
+        correct: 'Butterflies',
+        questionId: 4
+      },
+      {
+        question:
+          'How many pairs of wings do bees have?',
+        answers: ['Eleven', 'Four', 'Two', 'One'],
+        correct: 'Two',
+        questionId: 5
+      },
+      {
+        question:
+          'What is a doe?',
+        answers: ['What pizza is made of', 'Female deer', 'Space behind a frogs knees', 'Bear toes'],
+        correct: 'Female deer',
+        questionId: 6
+      },
+      {
+        question:
+          'What is a group of lions called?',
+        answers: ['a pack', 'a liars', 'a beast', 'a pride'],
+        correct: 'a pride',
+        questionId: 7
+      },
+      {
+        question:
+          'What’s the first thing a caterpillar usually eats after it’s born?',
+        answers: ['a pack', 'a liars', 'a beast', 'a pride'],
+        correct: 'a pride',
+        questionId: 8
+      },
+      {
+        question:
+          'The trumpeter swan—North America’s largest waterfowl—weighs about how many pounds?',
+        answers: ['30', '10', '50', '70'],
+        correct: '30',
+        questionId: 9
+      },
+      {
+        question:
+          'Which of the following animals is not nocturnal?',
+        answers: ['Skunk', 'Leopard', 'Tiger', 'Sheep'],
+        correct: 'Sheep',
+        questionId: 10
+      },
+      {
+        question:
+          'Walruses can eat about how many clams in one day?',
+        answers: ['5,000', '10,000', '12,000', '16,000'],
+        correct: '5,000',
+        questionId: 11
+      },
+      {
+        question:
+          'What is a baby rabbit called?',
+        answers: ['Doe', 'Buck', 'Kit','Hare'],
+        correct: 'Kit',
+        questionId: 12
+      },
+      {
+        question:
+          'How many koalas stacked on top of one another does it take to reach the height of a male giraffe—the world’s tallest land animal?',
+        answers: ['7','2', '4', '50'],
+        correct: '7',
+        questionId: 13
+      },
+      {
+        question:
+          'An ant says, “Danger ahead!” by doing what?',
+        answers: ['squealing', 'running toward water', 'rolling on its back', ' oozing chemicals'],
+        correct: ' oozing chemicals',
+        questionId: 14
+      },
+      {
+        question:
+          'Which of the following is not a type of penguin?',
+        answers: ['waterland','emperor', 'chinstrap', 'macaroni'],
+        correct: 'waterland',
+        questionId: 15
       },
 
 function Quiz(props) {
