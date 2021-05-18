dcfe34bd55bf2c6f9de01397713eb42109196cbd
Susana Martins <scmartins87@gmail.com>
2021-05-18 09:56:26 -0700

    adding animal questions

---
+++ src/components/Quiz/Quiz.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const animalQuestions = [
+    {
+      question:
+        'How many legs does a spider have?',
+      answers: ['Six', 'Eight', 'Four', 'Two'],
+      correct: 'vinayak',
+      questionId: '1'
+    },
+    {
+        question:
+          'What’s the name of a place you go to see lots of animals?',
+        answers: ['Park', 'School', 'Bus', 'Zoo'],
+        correct: 'zoo',
+        questionId: '2'
+      },
+
+function Quiz(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Quiz;
