76a79eb940aa8a205e0e30a930ce437551112e91
Joshua <millerjoshua737@gmail.com>
2021-05-19 14:09:49 -0500

    render error fixed

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -5,13 +5,29 @@ import NavBar from '../../components/NavBar/NavBar';
 import APIurl from '../../config'
 import TheContext from '../../context/index'
 
+
 const MainPage = () => {
+  const [start, setStart] = useState(false)
   const [randomQuestion, setRandomQuestion] = useState({});
   const [toggleButton, setToggleButton] = useState(true);
   const [score, setScore] = useState(0);
   const [timer, setTimer] = useState(0);
   const context = useContext(TheContext)
   // const [category, setCategory] = useState('')
+
+
+  useEffect(() => {
+    (async() => {
+      try {
+        const res = await axios.get(`https://quisbee.herokuapp.com/trivia`);
+        const response = res.data.filter(question => question.category == context.category)
+        setRandomQuestion(response[Math.floor(Math.random() * response.length)])
+        console.log(response)
+      } catch (err) {
+        console.error(err)
+      }
+    })()
+  }, [])
 
   const handleNextQues = () => {
     setToggleButton(!toggleButton)
@@ -25,6 +41,10 @@ const MainPage = () => {
       }
   }
 
+  const handleStart = () => {
+    setStart(true)
+  }
+
   //click the right choice
     //+= 100
   //click wrong choice
@@ -33,26 +53,15 @@ const MainPage = () => {
     //+= 50
 
   const correctChoice = () => {
-    setScore(+100)
+    setScore(score + 100)
     handleNextQues()
   }
 
   const incorrectChoice = () => {
-    setScore(-50)
+    setScore(score - 50)
   }
 
-  useEffect(() => {
-    (async() => {
-      try {
-        const res = await axios.get(`https://quisbee.herokuapp.com/trivia`);
-        const response = res.data.filter(question => question.category == context.category)
-        setRandomQuestion(response[Math.floor(Math.random() * response.length)])
-        console.log(response)
-      } catch (err) {
-        console.error(err)
-      }
-    })()
-  }, [toggleButton])
+
   
   return (
     <div>
@@ -69,25 +78,30 @@ const MainPage = () => {
           </div>
         </div>
       </div>
-      <div className={styles['question-container']}>
-        <h1 className={styles['render-question']}>
-          {randomQuestion.question}
-        </h1>
-        <div className={styles['choices-btns']}>
-          {
-            
-            randomQuestion.answers.map((answer) => {
-              return (
-                <button onClick={handleScore} value={answer}>{answer}</button>
-              )
-            })
-            
-          }
+      {
+        start ?
+        <div className={styles['question-container']}>
+          <h1 className={styles['render-question']}>
+            {randomQuestion.question}
+          </h1>
+          <div className={styles['choices-btns']}>
+            {
+              
+              randomQuestion.answers.map((answer) => {
+                return (
+                  <button onClick={handleScore} value={answer}>{answer}</button>
+                )
+              })
+              
+            }
+          </div>
+          <button className={styles['next-btn']} onClick={handleNextQues}>
+            <p className={styles['next-question']}>NEXT QUESTION</p>
+          </button>
         </div>
-        <button className={styles['next-btn']} onClick={handleNextQues}>
-          <p className={styles['next-question']}>NEXT QUESTION</p>
-        </button>
-      </div>
+        :
+        <button onClick={handleStart}>Start!</button>
+      }
     </div>
   )
 }
