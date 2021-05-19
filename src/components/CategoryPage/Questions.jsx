54fd2e94dfa0073adf764ac0d8804508a31474fc
ghernandez24 <halex24a@bu.edu>
2021-05-18 22:05:53 -0400

    added config

---
+++ src/components/CategoryPage/Questions.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import APIurl from '../../config'
+import axios from 'axios'
+
+const Questions = () => {
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(() => {
+        axios(`${APIurl}/trivia`).then((res) => setQuestions(res.data)).catch(console.error) 
+    }, [])
+    if (!questions.length){
+        console.log(questions)
+        return <h1> loading...</h1>
+    }
+
+    return (
+        <div>
+            {questions.map((question) => {
+                return <h3> {question.question} </h3>
+            })}
+        </div>
+    );
+};
+
+export default Questions;
